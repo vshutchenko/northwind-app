8db1a67ccf8355e2ace972310deb3b29d5607315
Vlad Shutchenko <vshutchenko@mail.ru>
2022-03-27 01:34:09 +0300

    Fix nuget packages. Add controllers

---
+++ module06/NorthwindMvcApp/Controllers/BlogCommentEntitiesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Services.EntityFrameworkCore.Blogging.Context;
+using Northwind.Services.EntityFrameworkCore.Blogging.Entities;
+
+namespace NorthwindMvcApp.Controllers
+{
+    public class BlogCommentEntitiesController : Controller
+    {
+        private readonly BloggingContext _context;
+
+        public BlogCommentEntitiesController(BloggingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BlogCommentEntities
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BlogComments.ToListAsync());
+        }
+
+        // GET: BlogCommentEntities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogCommentEntity = await _context.BlogComments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (blogCommentEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(blogCommentEntity);
+        }
+
+        // GET: BlogCommentEntities/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BlogCommentEntities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Text,Posted,AuthorId,ArticleId")] BlogCommentEntity blogCommentEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(blogCommentEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blogCommentEntity);
+        }
+
+        // GET: BlogCommentEntities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogCommentEntity = await _context.BlogComments.FindAsync(id);
+            if (blogCommentEntity == null)
+            {
+                return NotFound();
+            }
+            return View(blogCommentEntity);
+        }
+
+        // POST: BlogCommentEntities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Text,Posted,AuthorId,ArticleId")] BlogCommentEntity blogCommentEntity)
+        {
+            if (id != blogCommentEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(blogCommentEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BlogCommentEntityExists(blogCommentEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blogCommentEntity);
+        }
+
+        // GET: BlogCommentEntities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogCommentEntity = await _context.BlogComments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (blogCommentEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(blogCommentEntity);
+        }
+
+        // POST: BlogCommentEntities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var blogCommentEntity = await _context.BlogComments.FindAsync(id);
+            _context.BlogComments.Remove(blogCommentEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BlogCommentEntityExists(int id)
+        {
+            return _context.BlogComments.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ module06/NorthwindMvcApp/Controllers/ProductEntitiesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Services.EntityFrameworkCore.Context;
+using Northwind.Services.EntityFrameworkCore.Entities;
+
+namespace NorthwindMvcApp.Controllers
+{
+    public class ProductEntitiesController : Controller
+    {
+        private readonly NorthwindContext _context;
+
+        public ProductEntitiesController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProductEntities
+        public async Task<IActionResult> Index()
+        {
+            var northwindContext = _context.Products.Include(p => p.Category).Include(p => p.Supplier);
+            return View(await northwindContext.ToListAsync());
+        }
+
+        // GET: ProductEntities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productEntity = await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(productEntity);
+        }
+
+        // GET: ProductEntities/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "SupplierId", "CompanyName");
+            return View();
+        }
+
+        // POST: ProductEntities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,SupplierId,CategoryId,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] ProductEntity productEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", productEntity.CategoryId);
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "SupplierId", "CompanyName", productEntity.SupplierId);
+            return View(productEntity);
+        }
+
+        // GET: ProductEntities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productEntity = await _context.Products.FindAsync(id);
+            if (productEntity == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", productEntity.CategoryId);
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "SupplierId", "CompanyName", productEntity.SupplierId);
+            return View(productEntity);
+        }
+
+        // POST: ProductEntities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,SupplierId,CategoryId,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] ProductEntity productEntity)
+        {
+            if (id != productEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductEntityExists(productEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", productEntity.CategoryId);
+            ViewData["SupplierId"] = new SelectList(_context.Suppliers, "SupplierId", "CompanyName", productEntity.SupplierId);
+            return View(productEntity);
+        }
+
+        // GET: ProductEntities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productEntity = await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(productEntity);
+        }
+
+        // POST: ProductEntities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productEntity = await _context.Products.FindAsync(id);
+            _context.Products.Remove(productEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductEntityExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
