a1fb7377c723b50bd1a036815219f0151a03bb7c
Vlad Shutchenko <vshutchenko@mail.ru>
2022-04-20 04:19:01 +0300

    Add authorization policies

---
+++ module06/NorthwindMvcApp/Authorization/ArticleAccessAuthorizationHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Northwind.Services.Blogging;
+using NorthwindMvcApp.Extensions;
+using System;
+using System.Threading.Tasks;
+
+namespace NorthwindMvcApp.Authorization
+{
+    public class ArticleAccessAuthorizationHandler : AuthorizationHandler<OperationAuthorizationRequirement, BlogArticle>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OperationAuthorizationRequirement requirement, BlogArticle resource)
+        {
+            if (context.User == null || resource == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (context.User.IsAdmin() || context.User.IsAuthor(resource.AuthorId.ToString()))
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ module06/NorthwindMvcApp/Authorization/CommentAccessAuthorizationHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Northwind.Services.Blogging;
+using NorthwindMvcApp.Extensions;
+using System.Threading.Tasks;
+
+namespace NorthwindMvcApp.Authorization
+{
+    public class CommentAccessAuthorizationHandler : AuthorizationHandler<OperationAuthorizationRequirement, BlogComment>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OperationAuthorizationRequirement requirement, BlogComment resource)
+        {
+            if (context.User == null || resource == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (context.User.IsAdmin() || context.User.IsAuthor(resource.AuthorId.ToString()))
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
